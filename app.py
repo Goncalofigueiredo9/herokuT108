86df412cda35c74560095611ed9372ebc549281a
Goncalofigueiredo9 <49910977+Goncalofigueiredo9@users.noreply.github.com>
2019-06-17 11:44:29 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,21 +1,141 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import psycopg2
 app = Flask(__name__)
+
+
+@app.route('/carrinho')
+def carrinho():
+    return render_template('carrinho.html')
+
+
+def herokudb ():
+    Host='ec2-54-228-207-163.eu-west-1.compute.amazonaws.com'
+    Database='da90t09u9psuds'
+    User='ulgzcyduiykfwy'
+    Password='3f52a9d0c4b06dad5a314cd176ec2aa08c8ebb8fae6cc1c8003e2de3bdc038a9'
+    return psycopg2.connect(host=Host, database=Database, user=User, password=Password, sslmode='require')
+
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/login')
+
+
+def gravar(v1, v2, v3):
+    ficheiro = herokudb()
+    db = ficheiro.cursor()
+    db.execute("CREATE TABLE IF NOT EXISTS usr (nome text,email text, passe text)")
+    db.execute("INSERT INTO usr VALUES (%s, %s, %s)", (v1, v2, v3))
+    ficheiro.commit()
+    ficheiro.close()
+
+
+def existe(v1):
+    try:
+        ficheiro = herokudb()
+        db = ficheiro.cursor()
+        db.execute("SELECT * FROM usr WHERE nome = %s", (v1,))
+        valor = db.fetchone()
+        ficheiro.close()
+    except:
+        valor=None
+    return valor
+
+
+
+def log(v1, v2):
+    ficheiro = herokudb()
+    db = ficheiro.cursor()
+    db.execute("SELECT * FROM usr WHERE nome = %s and passe = %s", (v1, v2,))
+    valor = db.fetchone()
+    ficheiro.close()
+    return valor
+
+
+@app.route('/registo', methods=['GET', 'POST'])
+def route():
+    erro = None
+    if request.method == 'POST':
+        v1 = request.form['utilizador']
+        v2 = request.form['email']
+        v3 = request.form['passe']
+        v4 = request.form['cpasse']
+        if existe(v1):
+            erro = 'O Utilizador já existe.'
+        elif v3 != v4:
+            erro = 'A palavra passe não coincide.'
+
+        else:
+            gravar(v1, v2, v3)
+    return render_template('registo.html', erro=erro)
+
+
+def alterar(v1, v2):
+    ficheiro = herokudb()
+    db = ficheiro.cursor()
+    db.execute("UPDATE usr SET passe = %s WHERE nome = %s", (v2, v1))
+    ficheiro.commit()
+    ficheiro.close()
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    erro = None
+    if request.method == 'POST':
+        v1 = request.form['utilizador']
+        v2 = request.form['passe']
+        if not existe(v1):
+            erro = 'O Utilizador não existe.'
+        elif not log(v1, v2):
+            erro = 'A palavra passe está errada.'
+        else:
+            erro = 'Bem-Vindo.'
+    return render_template('login.html', erro=erro)
 
-@app.route('/registo')
-def registo():
-    return render_template('registo.html')
 
-@app.route('/carrinho')
-def carrinho():
-    return render_template('carrinho.html')
+
+@app.route('/newpasse', methods=['GET', 'POST'])
+def newpasse():
+    erro = None
+    if request.method == 'POST':
+        v1 = request.form['utilizador']
+        v2 = request.form['passe']
+        v3 = request.form['cpasse']
+        if not existe(v1):
+            erro = 'O Utilizador não existe.'
+        elif v2 != v3:
+            erro = 'A palavra passe não coincide.'
+        else:
+            alterar(v1, v2)
+    return render_template('newpasse.html', erro=erro)
+
+
+def eliminar(v1):
+    ficheiro = herokudb()
+    db = ficheiro.cursor()
+    db.execute("DELETE FROM usr WHERE nome = %s", (v1,))
+    ficheiro.commit()
+    ficheiro.close()
+    return
+
+@app.route('/apagar', methods=['POST', 'GET'])
+def apagar():
+    erro = None
+    if request.method == "POST":
+        v1 = request.form['usr']
+        v2 = request.form['pwd']
+        if not existe(v1):
+            erro = 'O Utilizador não existe.'
+        elif not log(v1, v2):
+            erro = 'A senha está incorreta.'
+        else:
+            eliminar(v1)
+            erro = 'Conta eliminada com Sucesso.'
+    return render_template('apagar.html', erro=erro)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
